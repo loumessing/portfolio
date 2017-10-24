d59f29430f5e53ff1be3e527ca6157ec45639763
loumessing <loumessing@gmail.com>
2017-10-24 07:24:35 -0600

    Assignment-2-listchat:completed

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,10 @@
+(function(){
+  function HomeCtrl(Room){
+    this.rooms = Room.all;
+console.log(this.rooms)
+  }
+  angular
+    .module('blocChat')
+    .controller('HomeCtrl', ['Room', HomeCtrl]);
+
+})();
